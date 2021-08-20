18bc3b918e5bc725426b980f5f25ad9f724b6d89
MboyaDan <45758432+MboyaDan@users.noreply.github.com>
2021-08-20 17:51:39 +0300

    home screen

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:taskmanager/main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text(MyApp.title),),
+    );
+  }
+}
